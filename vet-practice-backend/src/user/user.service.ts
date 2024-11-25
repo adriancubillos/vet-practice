a7f8d7486c2be4d3d46228c94ff76ee925d42056
Adrian Cubillos <adriancubillos@gmail.com>
2024-11-25 15:58:08 -0500

    implement user service

--- vet-practice-backend/src/user/user.service.ts
+++ vet-practice-backend/src/user/user.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(createUserDto: CreateUserDto): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+    const user = this.usersRepository.create({
+      username: createUserDto.username,
+      password: hashedPassword,
+    });
+    return this.usersRepository.save(user);
+  }
+
+  async findUserByUsername(username: string): Promise<User> {
+    return this.usersRepository.findOne({ where: { username } });
+  }
+
+  async generateJwt(user: User): Promise<string> {
+    return this.jwtService.sign({ id: user.id, username: user.username });
+  }
+}
