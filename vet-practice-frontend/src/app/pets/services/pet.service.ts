23d8f432f644ba4c0b9670b87fc96d508b69b9fe
Adrian Cubillos <adriancubillos@gmail.com>
2024-11-26 17:23:59 -0500

    use backend service to register pet

--- vet-practice-frontend/src/app/pets/services/pet.service.ts
+++ vet-practice-frontend/src/app/pets/services/pet.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Pet, PetRegistrationRequest, MedicalRecord } from '../models/pet.interface';
@@ -8,45 +8,57 @@ import { Pet, PetRegistrationRequest, MedicalRecord } from '../models/pet.interf
   providedIn: 'root'
 })
 export class PetService {
-  private readonly baseUrl = `${environment.apiUrl}/pets`;
+  private readonly baseUrl = `${environment.apiUrl.replace('/auth', '')}/pets`;
+  private readonly TOKEN_KEY = environment.tokenKey;
 
   constructor(private http: HttpClient) {}
 
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem(this.TOKEN_KEY);
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
   // Pet CRUD operations
   registerPet(formData: FormData): Observable<Pet> {
-    return this.http.post<Pet>(this.baseUrl, formData);
+    return this.http.post<Pet>(this.baseUrl, formData, { headers: this.getHeaders() });
   }
 
   getPets(): Observable<Pet[]> {
-    return this.http.get<Pet[]>(this.baseUrl);
+    return this.http.get<Pet[]>(this.baseUrl, { headers: this.getHeaders() });
+  }
+
+  getAllPets(): Observable<Pet[]> {
+    return this.http.get<Pet[]>(`${this.baseUrl}/all`, { headers: this.getHeaders() });
   }
 
   getPetById(id: number): Observable<Pet> {
-    return this.http.get<Pet>(`${this.baseUrl}/${id}`);
+    return this.http.get<Pet>(`${this.baseUrl}/${id}`, { headers: this.getHeaders() });
   }
 
   updatePet(id: number, pet: Partial<Pet>): Observable<Pet> {
-    return this.http.patch<Pet>(`${this.baseUrl}/${id}`, pet);
+    return this.http.patch<Pet>(`${this.baseUrl}/${id}`, pet, { headers: this.getHeaders() });
   }
 
   deletePet(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+    return this.http.delete<void>(`${this.baseUrl}/${id}`, { headers: this.getHeaders() });
   }
 
   // Medical History operations
   addMedicalRecord(petId: number, record: Omit<MedicalRecord, 'id' | 'petId'>): Observable<MedicalRecord> {
-    return this.http.post<MedicalRecord>(`${this.baseUrl}/${petId}/medical-records`, record);
+    return this.http.post<MedicalRecord>(`${this.baseUrl}/${petId}/medical-records`, record, { headers: this.getHeaders() });
   }
 
   getMedicalRecords(petId: number): Observable<MedicalRecord[]> {
-    return this.http.get<MedicalRecord[]>(`${this.baseUrl}/${petId}/medical-records`);
+    return this.http.get<MedicalRecord[]>(`${this.baseUrl}/${petId}/medical-records`, { headers: this.getHeaders() });
   }
 
   updateMedicalRecord(petId: number, recordId: number, record: Partial<MedicalRecord>): Observable<MedicalRecord> {
-    return this.http.patch<MedicalRecord>(`${this.baseUrl}/${petId}/medical-records/${recordId}`, record);
+    return this.http.patch<MedicalRecord>(`${this.baseUrl}/${petId}/medical-records/${recordId}`, record, { headers: this.getHeaders() });
   }
 
   deleteMedicalRecord(petId: number, recordId: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${petId}/medical-records/${recordId}`);
+    return this.http.delete<void>(`${this.baseUrl}/${petId}/medical-records/${recordId}`, { headers: this.getHeaders() });
   }
 }
