3d56e2d64a5f96f2ef448764796394f2d2a43da3
Adrian Cubillos <adriancubillos@gmail.com>
2024-11-26 08:32:18 -0500

    Udpadint routing to include login and register routes redirects now by default to login page

--- vet-practice-frontend/src/app/app.routes.ts
+++ vet-practice-frontend/src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/components/register/register.component';
+import { LoginComponent } from './auth/components/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './auth/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { 
     path: 'dashboard', 
     component: DashboardComponent,
     canActivate: [authGuard]
   },
-  { path: '**', redirectTo: 'register' } // Catch all route
+  { path: '**', redirectTo: 'login' } // Catch all route
 ];
